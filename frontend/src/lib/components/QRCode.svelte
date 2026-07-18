9a8021c738cd889fd06adf57e8e82ec98deb66d2
JerryInd <rebels.21.1995@gmail.com>
2026-07-18 16:18:01 +0530

    QRCode component: support both src (WA image) and data (TG client-side render)

--- frontend/src/lib/components/QRCode.svelte
+++ frontend/src/lib/components/QRCode.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
-  let { src = '' }: { src: string } = $props();
+  import qrcode from 'qrcode';
+
+  let { src = '', data = '' }: { src?: string; data?: string } = $props();
+  let qrImage = $state('');
+
+  $effect(() => {
+    if (src) {
+      qrImage = src;
+    } else if (data) {
+      qrcode.toDataURL(data, { errorCorrectionLevel: 'L', margin: 2, width: 300 }, (err: any, url: string) => {
+        if (!err) qrImage = url;
+      });
+    } else {
+      qrImage = '';
+    }
+  });
 </script>
 
-{#if src}
-  <img src={src} alt="QR Code" width="256" height="256" style="image-rendering: pixelated;" />
+{#if qrImage}
+  <img src={qrImage} alt="QR Code" width="256" height="256" style="image-rendering: pixelated;" />
 {/if}
